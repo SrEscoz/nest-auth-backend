8b98a840ac0e5cf6b7c965c2c6171a88c27a6c28
Escoz <kyrovilches@gmail.com>
2025-01-15 14:14:56 +0100

    Añadida generación del token JWT

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,15 +1,16 @@
 import {
-  Controller,
-  Get,
-  Post,
   Body,
-  Patch,
-  Param,
+  Controller,
   Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
 } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { CreateUserDto } from './dto/create-user-dto';
+import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateAuthDto } from './dto/update-auth.dto';
+import { LoginDto } from './dto/login-user.dto';
 
 @Controller('auth')
 export class AuthController {
@@ -20,14 +21,19 @@ export class AuthController {
     return this.authService.create(createUserDto);
   }
 
+  @Post('/login')
+  login(@Body() loginDto: LoginDto) {
+	  return this.authService.login(loginDto)
+  }
+
   @Get()
   findAll() {
     return this.authService.findAll();
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.authService.findOne(+id);
+  findOne(@Param('id') email: string) {
+    return this.authService.findOne(email);
   }
 
   @Patch(':id')
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,20 +2,26 @@ import {
   BadRequestException,
   Injectable,
   InternalServerErrorException,
+  NotFoundException,
+  UnauthorizedException,
 } from '@nestjs/common';
-import { CreateUserDto } from './dto/create-user-dto';
+import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateAuthDto } from './dto/update-auth.dto';
 import { User } from './entities/user.entity';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 
 import * as bcryptjs from 'bcryptjs';
+import { LoginDto } from './dto/login-user.dto';
+import { JwtService } from '@nestjs/jwt';
+import { JwtPayload } from './interfaces/jwt-payload';
 
 @Injectable()
 export class AuthService {
   constructor(
     @InjectModel(User.name)
     private userModel: Model<User>,
+    private jwtService: JwtService,
   ) {}
 
   async create(createUserDto: CreateUserDto): Promise<User> {
@@ -28,7 +34,6 @@ export class AuthService {
       });
 
       return await newUser.save();
-
     } catch (error) {
       if (error.code === 11000) {
         // Entrada duplicada
@@ -39,12 +44,38 @@ export class AuthService {
     }
   }
 
+  async login(loginDto: LoginDto) {
+    const { email, password } = loginDto;
+
+    const [user] = await Promise.all([this.userModel.findOne({ email })]);
+
+    if (!user) {
+      throw new UnauthorizedException('Invalid credentials');
+    }
+
+    if (!bcryptjs.compareSync(password, user.password)) {
+      throw new UnauthorizedException('Invalid credentials');
+    }
+
+    const { password: _, ...rest } = user.toJSON();
+    return {
+      user: rest,
+      token: this.getJwtToken({ id: user.id }),
+    };
+  }
+
   findAll() {
     return `This action returns all auth`;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} auth`;
+  async findOne(email: string): Promise<User> {
+    const [user] = await Promise.all([this.userModel.findOne({ email })]);
+
+    if (!user) {
+      throw new NotFoundException(`User with email ${email} not found`);
+    }
+
+    return user;
   }
 
   update(id: number, updateAuthDto: UpdateAuthDto) {
@@ -53,5 +84,9 @@ export class AuthService {
 
   remove(id: number) {
     return `This action removes a #${id} auth`;
+  }
+
+  getJwtToken(payload: JwtPayload) {
+    return this.jwtService.sign(payload);
   }
 }
