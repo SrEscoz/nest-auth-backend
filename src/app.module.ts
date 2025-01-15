9acad21385e816452aa73441dc75d633f31b1fdb
Escoz <kyrovilches@gmail.com>
2025-01-15 10:08:40 +0100

    Añadidas variables de entorno y conexión a MongoAtlas

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
+import * as process from 'node:process';
 
 @Module({
-  imports: [AuthModule],
   controllers: [],
+  imports: [
+    AuthModule,
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.MONGO_URI),
+  ],
   providers: [],
 })
 export class AppModule {}
